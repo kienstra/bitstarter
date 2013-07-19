827fac3c7dc4d9fca0c765a62677d34bef4c2d8b
kienstra <kienstraryan@gmail.com>
2013-07-19 21:17:37 +0000

    sdf

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 
-var here = buf.toString(fs.readFileSync('/bitstarter/index.html', function (err, data) {
+var here = buf.toString(fs.readFileSync('index.html', function (err, data) {
   if (err) throw err;
   console.log(data);
 }));
@@ -9,7 +9,7 @@ var here = buf.toString(fs.readFileSync('/bitstarter/index.html', function (err,
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync('index.html')));
+  response.send(buf.toString(fs.readFileSync('/bitstarter/index.html')));
 });
 
 var port = process.env.PORT || 5000;
