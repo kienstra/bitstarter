caaa3729d398ee63ec35f5348043e1eb1ba07d59
kienstra <kienstraryan@gmail.com>
2013-10-16 21:48:02 +0000

    will this work?

--- web.js
+++ web.js
@@ -10,7 +10,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send(buffed); //used to be buffed;
-
+}
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
