b30e718af39224c7fb91b9a5ae7f9e20d84e1b97
kienstra <kienstraryan@gmail.com>
2013-10-17 17:26:04 +0000

    Edited title

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 
-var random = fs.readFileSync('index2.html')
+var random = fs.readFileSync('index.html')
 
 
 buffed = random.toString();
@@ -10,8 +10,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send(buffed); //used to be buffed;
-}
-var port = process.env.PORT || 5000;
+});
+var port = process.env.PORT || 8080; //used to be 5000: 8080 is a public port
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
