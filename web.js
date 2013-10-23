c72f592ec13d8e293fbab9fdc43214428467efd2
kienstra <kienstra>
2013-10-23 23:53:57 +0000

    Another change

--- web.js
+++ web.js
@@ -11,12 +11,9 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 
   response.send(buffed); //used to be buffed;
-})
+});
+  
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-
-  response.send(buffed);
-
 });
-
