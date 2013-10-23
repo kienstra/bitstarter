daea4de00babe926d0c97600eda3e6b96d9728d2
kienstra <kienstra>
2013-10-23 23:46:47 +0000

    Fixed something in this branch

--- web.js
+++ web.js
@@ -1,26 +1,24 @@
 var express = require('express');
 var fs = require('fs');
 
-<<<<<<< HEAD
-var random = fs.readFileSync('index.html')
-=======
+
 var random = fs.readFileSync('index.html');
->>>>>>> staging
+var random = fs.readFileSync('index.html');
 
 var buffed = random.toString();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-<<<<<<< HEAD
+
   response.send(buffed); //used to be buffed;
 })
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-=======
+
   response.send(buffed);
->>>>>>> staging
+
 });
 
 var port = process.env.PORT || 8080;
