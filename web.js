3b434a65b45e36ac7a252b6979b0197a6ededa49
kienstra <kienstraryan@gmail.com>
2014-01-14 21:04:27 -0800

    Aligned

--- web.js
+++ web.js
@@ -1,19 +1,13 @@
 var express = require('express');
 var fs = require('fs');
-
-
-var random = fs.readFileSync('index.html');
-
-var buffed = random.toString();
-
+var buffed = fs.readFileSync('index.html').toString();
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-
-  response.send(buffed); //used to be buffed;
+app.get('/', function( request, response) {
+    response.send(buffed);
 });
-  
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+
+var port = process.env.PORT || 8888;
+app.listen( port , function() {
+    console.log( 'Listening on ' + port );
 });
